a59928674bd520b50c63847cdc0196207aad8050
Wakidou <wakidou.abdallah@gmail.com>
2022-02-25 13:23:36 +0100

    Initial commit

---
+++ testUnitJs/src/tests/App.test.tsx
@@ -0,0 +1,143 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import ReactDOM from "react-dom";
+import { act } from "react-dom/test-utils";
+import App from "../App";
+
+let container: any;
+
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+test("renders have calculatrice", () => {
+  const { container } = render(<App />);
+  const title = screen.getByText(/calculatrice/i);
+  expect(title).toBeInTheDocument();
+  expect(container.getElementsByClassName("touch").length).toBe(16);
+});
+
+test("test addition fonctional", () => {
+  act(() => {
+    ReactDOM.render(<App />, container);
+  });
+  const button1 = container.querySelector(".touch[data-value='1']");
+  const button2 = container.querySelector(".touch[data-value='2']");
+  const buttonPlus = container.querySelector(".touch[data-value='+']");
+  const buttonEqual = container.querySelector(".touch[data-value='=']");
+  const label = container.querySelector(".result");
+  act(() => {
+    button1.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+  act(() => {
+    button2.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+  act(() => {
+    buttonPlus.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+  act(() => {
+    button1.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+  act(() => {
+    button2.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+  act(() => {
+    buttonEqual.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+
+  expect(label.textContent).toBe(Number(12 + 12).toString());
+});
+
+test("test substraction fonctional", () => {
+  act(() => {
+    ReactDOM.render(<App />, container);
+  });
+  const button1 = container.querySelector(".touch[data-value='1']");
+  const button2 = container.querySelector(".touch[data-value='2']");
+  const buttonMoins = container.querySelector(".touch[data-value='-']");
+  const buttonEqual = container.querySelector(".touch[data-value='=']");
+  const label = container.querySelector(".result");
+  act(() => {
+    button1.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+  act(() => {
+    button2.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+  act(() => {
+    buttonMoins.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+  act(() => {
+    button1.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+  act(() => {
+    button2.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+  act(() => {
+    buttonEqual.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+
+  expect(label.textContent).toBe(Number(12 - 12).toString());
+});
+
+test("test division fonctional", () => {
+  act(() => {
+    ReactDOM.render(<App />, container);
+  });
+  const button1 = container.querySelector(".touch[data-value='1']");
+  const button2 = container.querySelector(".touch[data-value='2']");
+  const buttonDivision = container.querySelector(".touch[data-value='/']");
+  const buttonEqual = container.querySelector(".touch[data-value='=']");
+  const label = container.querySelector(".result");
+  act(() => {
+    button1.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+  act(() => {
+    button2.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+  act(() => {
+    buttonDivision.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+  act(() => {
+    button1.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+  act(() => {
+    button2.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+  act(() => {
+    buttonEqual.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+
+  expect(label.textContent).toBe(Number(12 / 12).toString());
+});
+
+// test("test modulo fonctional", () => {
+//   act(() => {
+//     ReactDOM.render(<App />, container);
+//   });
+//   const button1 = container.querySelector(".touch[data-value='1']");
+//   const button2 = container.querySelector(".touch[data-value='2']");
+//   const buttonModulo = container.querySelector(".touch[data-value='%']");
+//   const buttonEqual = container.querySelector(".touch[data-value='=']");
+//   const label = container.querySelector(".result");
+//   act(() => {
+//     button1.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+//   });
+//   act(() => {
+//     button2.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+//   });
+//   act(() => {
+//     buttonModulo.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+//   });
+//   act(() => {
+//     button1.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+//   });
+//   act(() => {
+//     button2.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+//   });
+//   act(() => {
+//     buttonEqual.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+//   });
+
+//   expect(label.textContent).toBe(Number(12 % 12).toString());
+// });
---
+++ testUnitJs/src/tests/hooks/useCalculator.test.tsx
@@ -0,0 +1,20 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+import useCalculator from "../../hooks/useCalculator";
+
+test("test addition", () => {
+  const { result } = renderHook(() => useCalculator());
+  const { addition } = result.current;
+  expect(addition('1','1')).toEqual("2");
+});
+
+test("test substraction", () => {
+  const { result } = renderHook(() => useCalculator());
+  const { substraction } = result.current;
+  expect(substraction('1','1')).toEqual("0");
+});
+
+test("test division", () => {
+  const { result } = renderHook(() => useCalculator());
+  const { division } = result.current;
+  expect(division('1','1')).toEqual("1");
+});
